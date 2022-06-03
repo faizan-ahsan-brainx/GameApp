c30f47adb70d3ba71acfe72af3e2cea227c6314a
Faizan Ahsan <faizan.ahsan@brainxtech.com>
2022-06-03 11:15:14 +0500

    First Commit of Feature

--- GameApp/GameApp/Controllers/TodayViewController.swift
+++ GameApp/GameApp/Controllers/TodayViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  GameApp
 //
 //  Created by BrainX on 02/06/2022.
@@ -7,13 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodayViewController: UIViewController {
 
+    @IBOutlet var todayView: TodayView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        
+    }
+    
+
+  
 
 }
-
---
+++ GameApp/GameApp/Views/TodayView.swift
@@ -0,0 +1,17 @@
+//
+//  TodayView.swift
+//  GameApp
+//
+//  Created by BrainX on 03/06/2022.
+//
+
+import UIKit
+
+class TodayView: UIView {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var personImage: UIImageView!
+    
+    @IBOutlet weak var tableView: UITableView!
+}
